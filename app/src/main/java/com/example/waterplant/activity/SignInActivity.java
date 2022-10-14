160ceefa98ada78e54b4b9510e2906f800a4a496
Ahmad Reza <ahmadreza.crio@gmail.com>
2022-10-15 01:53:41 +0530

    Refactor Sign in screen

--- app/src/main/java/com/example/waterplant/activity/SignInActivity.java
+++ app/src/main/java/com/example/waterplant/activity/SignInActivity.java
@@ -1,32 +1,31 @@
 package com.example.waterplant.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.waterplant.R;
-import com.example.waterplant.utilities.ResourceUtility;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class LoginActivity extends AppCompatActivity {
-    private static final String USERNAME = "USERNAME";
-    private static final String PASSWORD = "PASSWORD";
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
 
-        // TODO We will fix it later
-//        String userName = ResourceUtility.getUserPreferences(this, LoginActivity.USERNAME);
-//        String password = ResourceUtility.getUserPreferences(this, LoginActivity.PASSWORD);
-
-        ImageView backBtn = findViewById(R.id.back_button);
+        Button backBtn = findViewById(R.id.back_button);
         backBtn.setOnClickListener(v -> onBackPressed());
+
+        Button forgotBtn = findViewById(R.id.forgot_button);
+        forgotBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
 
         TextInputLayout userNameLayout = findViewById(R.id.username_layout);
         TextInputEditText userNameInput = findViewById(R.id.username_input);
@@ -34,11 +33,10 @@ public class LoginActivity extends AppCompatActivity {
         TextInputLayout passwordLayout = findViewById(R.id.password_layout);
         TextInputEditText passwordInput = findViewById(R.id.password_input);
 
-        ExtendedFloatingActionButton signUpBtn = findViewById(R.id.signin_button);
-        signUpBtn.setOnClickListener(view -> {
+        ExtendedFloatingActionButton signInBtn = findViewById(R.id.signin_button);
+        signInBtn.setOnClickListener(view -> {
             String userName = userNameInput.getText() != null ? userNameInput.getText().toString() : "";
             String password = passwordInput.getText() != null ? passwordInput.getText().toString() : "";
-
             if (userName.isEmpty()) {
                 userNameLayout.setError(getString(R.string.require_field_error));
 
@@ -46,9 +44,20 @@ public class LoginActivity extends AppCompatActivity {
                 passwordLayout.setError(getString(R.string.require_field_error));
 
             } else {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
+                if (userName.equals("ahmad") && password.equals("ahmad//123")) {
+                    Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    passwordLayout.setError(getString(R.string.invalid_authorization_error));
+                }
             }
+        });
+
+        Button signUp = findViewById(R.id.signup_button);
+        signUp.setOnClickListener(view -> {
+            Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+            startActivity(intent);
         });
     }
 }
