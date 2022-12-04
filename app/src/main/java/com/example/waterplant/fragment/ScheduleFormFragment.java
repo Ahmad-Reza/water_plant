6f34afdfb9469465da1582b540bc02c8092a6d04
Ahmad Reza <ahmadreza.crio@gmail.com>
2022-12-04 10:59:57 +0530

    Added schedule form top action menu

--- app/src/main/java/com/example/waterplant/fragment/ScheduleFormFragment.java
+++ app/src/main/java/com/example/waterplant/fragment/ScheduleFormFragment.java
@@ -3,6 +3,7 @@ package com.example.waterplant.fragment;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,6 @@ import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 
 import com.example.waterplant.R;
-import com.example.waterplant.adapter.ActionListener;
 import com.example.waterplant.model.PlantModel;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
@@ -38,20 +38,25 @@ import java.util.Objects;
  * create an instance of this fragment.
  */
 public class ScheduleFormFragment extends BottomSheetDialogFragment {
-    private static final String PLANT_MODEL = "PLANT_MODEL";
+    private static final String ITEM_POSITION = "POSITION";
+    private static final String SCHEDULE_ITEM = "SCHEDULE_ITEM";
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d MMM 'at' hh:mm a");
 
     private PlantModel plantModel;
-    private LocalDateTime selectedDateTime;
-    private ActionListener<SchedulePlantModel> listener;
+    private SchedulePlantModel schedulePlantModel;
+
+    private int position;
+    private LocalDateTime scheduleTime;
+    private ActionListener<SchedulePlantModel> actionListener;
 
     public ScheduleFormFragment() { }
 
-    public static ScheduleFormFragment newInstance(PlantModel plantModel) {
+    public static ScheduleFormFragment newInstance(int position, Object item) {
         ScheduleFormFragment fragment = new ScheduleFormFragment();
         Bundle args = new Bundle();
-        args.putParcelable(PLANT_MODEL, plantModel);
+        args.putInt(ITEM_POSITION, position);
+        args.putParcelable(SCHEDULE_ITEM, (Parcelable) item);
         fragment.setArguments(args);
         return fragment;
     }
@@ -60,7 +65,13 @@ public class ScheduleFormFragment extends BottomSheetDialogFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            plantModel = getArguments().getParcelable(PLANT_MODEL);
+            position = getArguments().getInt(ITEM_POSITION);
+            Parcelable parcelable = getArguments().getParcelable(SCHEDULE_ITEM);
+            if (parcelable instanceof PlantModel) {
+                plantModel = (PlantModel) parcelable;
+            } else if (parcelable instanceof SchedulePlantModel) {
+                schedulePlantModel = (SchedulePlantModel) parcelable;
+            }
         }
     }
 
@@ -74,23 +85,30 @@ public class ScheduleFormFragment extends BottomSheetDialogFragment {
             if (itemId == R.id.close_button) {
                 dismiss();
                 return true;
+
+            } else if (itemId == R.id.remove_button) {
+                //TODO remove schedule plant
+                actionListener.onActionPerformed(ActionListener.Action.REMOVE, position, schedulePlantModel);
+
+                return true;
             }
             return false;
         });
 
         if (getContext() != null) {
             toolbar.getMenu().findItem(R.id.close_button).setIconTintList(AppCompatResources.getColorStateList(getContext(), R.color.white));
+            toolbar.getMenu().findItem(R.id.remove_button).setIconTintList(AppCompatResources.getColorStateList(getContext(), R.color.white));
         }
 
         TextInputLayout messageLayout = rootView.findViewById(R.id.message_layout);
         TextInputEditText messageInput = rootView.findViewById(R.id.message_input);
 
-        TextInputLayout dateTimeLayout = rootView.findViewById(R.id.date_time_layout);
-        TextInputEditText dateTimeInput = rootView.findViewById(R.id.date_time_input);
+        TextInputLayout dateTimeLayout = rootView.findViewById(R.id.schedule_time_layout);
+        TextInputEditText scheduleTimeInput = rootView.findViewById(R.id.schedule_time_input);
 
         LocalTime currentTime = LocalTime.now();
 
-        MaterialTimePicker materialTimePicker = new MaterialTimePicker.Builder()
+        MaterialTimePicker timePickerDialog = new MaterialTimePicker.Builder()
                 .setTimeFormat(TimeFormat.CLOCK_12H)
                 .setTitleText(R.string.time_picker_title)
                 .setInputMode(MaterialTimePicker.INPUT_MODE_CLOCK)
@@ -98,58 +116,73 @@ public class ScheduleFormFragment extends BottomSheetDialogFragment {
                 .setMinute(currentTime.getMinute())
                 .build();
 
-        materialTimePicker.addOnPositiveButtonClickListener(v -> {
-            int selectedHour = materialTimePicker.getHour();
-            int selectedMin = materialTimePicker.getMinute();
+        timePickerDialog.addOnPositiveButtonClickListener(v -> {
+            int selectedHour = timePickerDialog.getHour();
+            int selectedMin = timePickerDialog.getMinute();
 
-            selectedDateTime = selectedDateTime
-                    .withHour(selectedHour)
-                    .withMinute(selectedMin);
-
-            dateTimeInput.setText(formatDateTime(selectedDateTime, getResources()));
+            scheduleTime = scheduleTime.withHour(selectedHour).withMinute(selectedMin);
+            scheduleTimeInput.setText(formatDateTime(scheduleTime, getResources()));
         });
 
-        MaterialDatePicker<Long> materialDatePicker = MaterialDatePicker.Builder.datePicker()
+        MaterialDatePicker<Long> datePickerDialog = MaterialDatePicker.Builder.datePicker()
                 .setTitleText(R.string.date_picker_title)
                 .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
                 .build();
 
-        materialDatePicker.addOnPositiveButtonClickListener(selection -> {
+        datePickerDialog.addOnPositiveButtonClickListener(selection -> {
             if (selection != null) {
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTimeInMillis(selection);
 
-                selectedDateTime = calendar.getTime()
+                scheduleTime = calendar.getTime()
                         .toInstant()
                         .atZone(ZoneId.systemDefault())
                         .toLocalDateTime();
 
-                materialTimePicker.show(ScheduleFormFragment.this.getChildFragmentManager(), "MATERIAL_TIME_PICKER");
+                timePickerDialog.show(ScheduleFormFragment.this.getChildFragmentManager(), "MATERIAL_TIME_PICKER");
             }
         });
 
-        dateTimeInput.setOnClickListener(view -> {
-            if (!materialDatePicker.isAdded()) {
-                materialDatePicker.show(ScheduleFormFragment.this.getChildFragmentManager(), "MATERIAL_DATE_PICKER");
+        scheduleTimeInput.setOnClickListener(view -> {
+            if (!datePickerDialog.isAdded()) {
+                datePickerDialog.show(ScheduleFormFragment.this.getChildFragmentManager(), "MATERIAL_DATE_PICKER");
             }
         });
 
         ExtendedFloatingActionButton scheduleBtn = rootView.findViewById(R.id.schedule_button);
         scheduleBtn.setOnClickListener(view -> {
-            String message = Objects.requireNonNull(messageInput.getText()).toString();
-            String dateTime = Objects.requireNonNull(dateTimeInput.getText()).toString();
+            String inputMessage = Objects.requireNonNull(messageInput.getText()).toString();
+            String inputTime = Objects.requireNonNull(scheduleTimeInput.getText()).toString();
 
-            if (message.isEmpty()) {
+            if (inputMessage.isEmpty()) {
                 messageLayout.setError(getString(R.string.require_field_error));
 
-            } else if (dateTime.isEmpty()) {
+            } else if (inputTime.isEmpty()) {
                 dateTimeLayout.setError(getString(R.string.require_field_error));
 
             } else {
-                SchedulePlantModel schedulePlantModel = new SchedulePlantModel(plantModel, messageInput.getText().toString(), selectedDateTime);
-                listener.onActionPerformed(schedulePlantModel);
+                ActionListener.Action action;
+                if (schedulePlantModel != null) {
+                    action = ActionListener.Action.UPDATE;
+                    plantModel = schedulePlantModel.getPlantModel();
+                    scheduleTime = schedulePlantModel.getLocalDateTime();
+
+                } else action = ActionListener.Action.SAVE;
+
+                SchedulePlantModel updatedSchedulePlant = new SchedulePlantModel(plantModel, messageInput.getText().toString(), scheduleTime);
+
+                dismiss();
+                actionListener.onActionPerformed(action, position, updatedSchedulePlant);
             }
         });
+
+        if (schedulePlantModel != null) {
+            messageInput.setText(schedulePlantModel.getMessage());
+            scheduleTimeInput.setText(schedulePlantModel.getLocalDateTime().toString());
+
+        } else {
+            toolbar.getMenu().findItem(R.id.remove_button).setVisible(false);
+        }
 
         return rootView;
     }
@@ -157,25 +190,29 @@ public class ScheduleFormFragment extends BottomSheetDialogFragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-
-        listener = (ActionListener) context;
+        Fragment parentFragment = getParentFragment();
+        if (parentFragment != null) {
+            actionListener = (ActionListener) parentFragment;
+        } else {
+            actionListener = (ActionListener) context;
+        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        listener = null;
+        actionListener = null;
     }
 
     @Nullable
-    public static String formatDateTime(LocalDateTime dateTime, Resources resources) {
-        if (dateTime != null) {
-            StringBuilder selectedTime = new StringBuilder(dateTime.format(DATE_TIME_FORMATTER));
+    public static String formatDateTime(LocalDateTime scheduleTime, Resources resources) {
+        if (scheduleTime != null) {
+            StringBuilder selectedTime = new StringBuilder(scheduleTime.format(DATE_TIME_FORMATTER));
 
             LocalDate today = LocalDate.now();
             LocalDate tomorrow = today.plusDays(1);
             LocalDate yesterday = today.minusDays(2);
-            LocalDate scheduleDate = dateTime.toLocalDate();
+            LocalDate scheduleDate = scheduleTime.toLocalDate();
 
             if (yesterday.equals(scheduleDate)) {
                 selectedTime = selectedTime.replace(0, selectedTime.indexOf(" at"), resources.getString(R.string.yesterday_label));
